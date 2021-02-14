95ed6e86e8bfca7a45f4c28f5def52919a7c4aec
Kraig Johnson <johnson.kra@northeastern.edu>
2021-02-13 20:21:28 -0500

    added JUNIT test to show correctly child traversal

--- src/test/java/com/mindex/challenge/DataBootstrapTest.java
+++ src/test/java/com/mindex/challenge/DataBootstrapTest.java
@@ -54,6 +54,19 @@ public class DataBootstrapTest {
     }
 
     /**
+     * this is a test to ensure leaf nodes that have no children in the JSON tree will also return zero from the
+     * recursive traversal.
+     */
+    @Test
+    public void testReportingNullLeaves() {
+        Employee McCartney = employeeRepository.findByEmployeeId("b7839309-3348-463b-a7e3-5de1c168beb3");
+        EmployeeServiceImpl employeeService = new EmployeeServiceImpl(employeeRepository);
+        ReportingStructureImpl impl = new ReportingStructureImpl(employeeService);
+        assertEquals(0, impl.getTotalReports(McCartney));
+
+    }
+
+    /**
      * test to verify that a compensation can be created using data from an employee that was retrieved from the
      * database.
      */
@@ -63,7 +76,7 @@ public class DataBootstrapTest {
         int salary = 95000;
         String date = "2/12/2021";
         Compensation comp = new Compensation();
-        comp.setdate(date);
+        comp.seteffectiveDate(date);
         comp.setEmployee(lennon);
         comp.setSalary(salary);
         CompensationImpl compImpl = new CompensationImpl(compensationRepository);
